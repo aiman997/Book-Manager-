f9765dc9be6a276e77072cb59cfab18667158483
aiman997 <aimanelkhedrawi11@gmail.com>
2021-02-16 19:27:13 +0800

    Starting Flask APP

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template, make_response
+from sqlite import DB
+import sqlite3 as sql
+import sqlite3
 app = Flask(__name__)
+db=DB()
 
-@app.route('/hello')
-def hello_world():
-   return 'Hello World'
-   app.add_url_rule('/', 'hello', hello_world)
+db.open()
+try:
+    db.createTable()
+except:
+    print("Table already created before")
+
+@app.route('/addnew')
+def new_book():
+   return render_template('newbook.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            id = request.form['id']
+            name = request.form['name']
+            place = request.form['place']
+            authname = request.form['authname']
+            publish = request.form['publish']
+            msg = db.addBook(id, name, place, authname, publish)
+            print(msg)
+        except:
+             msg = "[flask][addrec] Major failure"
+        finally:
+            db.printTable()
+            db.close()
+        return render_template("result.html", msg = msg)
+
+@app.route('/list')
+def list():
+    con = sql.connect("datab.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from FlaskBooks")
+
+    rows = cur.fetchall();
+    return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- sqlite.py
+++ sqlite.py
@@ -1,47 +1,60 @@
 import sqlite3
 
 class DB:
-    file = "test.db"
+    file = 'datab.db'
     c = None
     con = None
 
     def open(self):
-      conn = sqlite3.connect(self.file)
+      conn = sqlite3.connect(self.file, check_same_thread=False)
       print("Opened database successfully")
       self.c = conn.cursor()
       self.con = conn
+
     def close(self):
         self.c.close()
         print("Connection to sql CLOSED")
 
     def createTable(self):
-      self.c.execute('''CREATE TABLE Books
-         (ID INT PRIMARY KEY    NOT NULL,
-         NAME           TEXT    NOT NULL,
-         AUTHOR         TEXT    NOT NULL);''')
+      self.c.execute('''CREATE TABLE FlaskBooks
+         (id INT PRIMARY KEY    NOT NULL,
+         name           TEXT    NOT NULL,
+         place          TEXT    NOT NULL,
+         authname       TEXT    NOT NULL,
+         publish        TEXT    NOT NULL);''')
       print("Table created successfully")
 
-    def addBook(self, id, name, author):
-         self.c.execute(f"INSERT INTO Books (ID,NAME,AUTHOR) VALUES ('{id}', '{name}', '{author}')")
-         self.con.commit()
+    def addBook(self, id, name, place, authname, publish):
+        try:
+            self.c.execute("INSERT INTO FlaskBooks (id,name,place,authname,publish) VALUES (?, ?, ?, ? ,?)", (id, name, place, authname, publish))
+            self.con.commit()
+            msg = "Record successfully added"
+        except Exception as e:
+            self.con.rollback()
+            msg = "Error in insert operation"
+            print(e)
+        return msg
+
 
     def printTable(self):
-        cursor = self.c.execute("SELECT * from Books")
+        cursor = self.c.execute("SELECT * from FlaskBooks")
         for row in cursor:
-            print("ID = ", row[0])
-            print("NAME = ", row[1])
-            print("AUTHOR = ", row[2])
+            print("id = ", row[0])
+            print("name = ", row[1])
+            print("place = ", row[2])
+            print("authname = ", row[3])
+            print("publish = ", row[4])
             print(row,"\n")
 
     def search(self,x):
-        cursor = self.c.execute(f"SELECT * FROM Books WHERE NAME = '{x}' OR ID = '{x}' OR AUTHOR = '{x}';")
+        cursor = self.c.execute(f"SELECT * FROM FlaskBooks WHERE id = '{x}' OR name = '{x}' OR place = '{x}' OR authname = '{x}' OR publish = '{x}';")
         for row in cursor:
             print("ID = ", row[0])
             print("NAME = ", row[1])
             print("AUTHOR = ", row[2])
 
-    def Update(self, id, updName, updAuthor ):
+    def Update(self, name, place, authname, publish):
         try:
-            cursor = self.c.execute(f"UPDATE Books SET NAME = '{updName}', AUTHOR = '{updAuthor}' WHERE BOOKID = '{id}';")
+            cursor = self.c.execute("UPDATE FlaskBooks SET (name,place,authname,publish) VALUES (?, ?, ? ,?)", ( name, place, authname, publish))
         except:
             print("An error occured during this Opration")
